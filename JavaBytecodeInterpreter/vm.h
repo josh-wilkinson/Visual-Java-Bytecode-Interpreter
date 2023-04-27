9b0ad2cd72ac588f1219248fb2b23d7113c0f88d
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2023-04-27 12:04:15 +0100

    Implemented vm reset and stack methods - push, pop

--- JavaBytecodeInterpreter/vm.h
+++ JavaBytecodeInterpreter/vm.h
@@ -3,11 +3,13 @@
 #define STACK_MAX 256
 
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <vector>
 
 // stack based interpreter
 struct
@@ -25,10 +27,32 @@ struct
 // opcodes enumerator
 typedef enum
 {
-
+	iconst_i,
+	iload,
+	istore,
+	iadd,
+	iinc,
+	isub,
+	imul,
+	ishl,
+	ishr,
+	if_icmpne,
+	if_icmpeq,
+	if_icmpgt,
+	if_cmpge,
+	if_icmplt,
+	if_icmple,
+	ifeq,
+	ifne,
+	ifgt,
+	ifge,
+	iflt,
+	ifle,
+	invokestatic,
+	invokevirtual
 } opcode;
 
-typedef enum interpretResult
+typedef enum class interpretResult
 {
 	SUCCESS,
 	ERROR_DIVISION_BY_ZERO,
@@ -37,24 +61,30 @@ typedef enum interpretResult
 
 void vmReset()
 {
-
+	std::cout << "Reset VM state" << std::endl;
+	vm = { NULL };
+	vm.stack_top = vm.stack;
 }
 
-void vmStackPush()
+void vmStackPush(uint64_t value)
 {
-
+	*vm.stack_top = value;
+	vm.stack_top++; // next space in memory
 }
 
 uint64_t vmStackPop()
 {
-	return NULL;
+	vm.stack_top--; // previous space in memory
+	return *vm.stack_top;
 }
 
 // interpreter code
-interpretResult vmInterpret()
+interpretResult vmInterpret() // program goes through code here
 {
 	vmReset();
 
-	return SUCCESS;
+
+
+	return interpretResult::SUCCESS;
 }
 
