05658bd4dfb4f720a488c0ce0de719296a69c508
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2023-05-01 19:13:21 +0100

    parseLine now adds to the code array the opcodes and operands in a line of code

--- JavaBytecodeInterpreter/parser.h
+++ JavaBytecodeInterpreter/parser.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "vm.h"
 
-void parseLine(std::string line, int code[256], int size)
+void parseLine(std::string line, int code[256], int &size)
 {
 	bool isCode = false;
 
@@ -15,8 +15,8 @@ void parseLine(std::string line, int code[256], int size)
 	for (int i = 0; i < line.length(); i++)
 	{
 		c = line.at(i);
-		//std::cout << c;
-		if (c == ':')
+
+		if (c == ':') // after line number
 		{
 			i = i + 2;
 			c = line.at(i);
@@ -29,8 +29,6 @@ void parseLine(std::string line, int code[256], int size)
 			opcodeOperand += c;
 		}
 	}
-
-	std::cout << opcodeOperand << std::endl;
 
 	// now parse the opcodeOperand string
 
@@ -48,14 +46,17 @@ void parseLine(std::string line, int code[256], int size)
 			if (itemCount == 1) // item 1: opcode
 			{
 				// add to array
+				code[size] = stringToOpcode(item);
+				size++;
 			}
 			else if (itemCount == 2) // item 2: possible operand
 			{
 				// add to array
+				code[size] = stoi(item);
+				size++;
 			}
 		}
-	}
-	std::cout << "Item count: " << itemCount << std::endl;
+	}	
 }
 
 void checkTokens()
@@ -63,38 +64,7 @@ void checkTokens()
 
 }
 
-opcode stringToOpcode(const std::string& str)
-{
-	if (str == "iconst_i") return iconst_i;
-	else if (str == "iload") return iload;
-	else if (str == "istore") return istore;
-	else if (str == "iadd") return iadd;
-	else if (str == "iinc") return iinc;
-	else if (str == "isub") return isub;
-	else if (str == "imul") return imul;
-	else if (str == "ishl") return ishl;
-	else if (str == "ishr") return ishr;
-	else if (str == "if_icmpne") return if_icmpne;
-	else if (str == "if_icmpeq") return if_icmpeq;
-	else if (str == "if_icmpgt") return if_icmpgt;
-	else if (str == "if_cmpge") return if_cmpge;
-	else if (str == "if_icmplt") return if_icmplt;
-	else if (str == "if_icmple") return if_icmple;
-	else if (str == "ifeq") return ifeq;
-	else if (str == "ifne") return ifne;
-	else if (str == "ifgt") return ifgt;
-	else if (str == "ifge") return ifge;
-	else if (str == "iflt") return iflt;
-	else if (str == "ifle") return ifle;
-	else if (str == "invokestatic") return invokestatic;
-	else if (str == "invokevirtual") return invokevirtual;
-	else if (str == "invokespecial") return invokespecial;
-	else if (str == "return") return OP_DONE;
-	else	
-		return NA;	
-}
-
-void read_instructions(int code[256], std::string filename, int size)
+void read_instructions(int code[256], std::string filename, int &size)
 {
 	// Ignore first line
 	// 2: class name + [ { ]
@@ -120,17 +90,8 @@ void read_instructions(int code[256], std::string filename, int size)
 	{
 		while (std::getline(myfile, line))
 		{
-			//read strings
-			itemReader.clear();
-			itemReader.str(line);
-			while (itemReader.good())
-			{
-				itemReader >> item;
-				if (item == "Code:")
-				{
-					parseLine(line, code, size);
-				}
-			}
+
+			parseLine(line, code, size);
 
 			currentLineNumber++;
 		}
